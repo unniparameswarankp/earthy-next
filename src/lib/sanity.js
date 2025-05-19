37a724ae7c6849127c5662277bc172e8aa1e177c
jo <jyothishkpk@gmail.com>
2025-05-19 12:38:05 +0530

    lkhfbahb

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,12 +1,12 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
+export const client = createClient({
   projectId: '9ixmdwm9',
   dataset: 'production',
   apiVersion: '2023-01-01',
-  useCdn: true
-})
+  useCdn: true,
+});
 
 const builder = imageUrlBuilder(client);
 
