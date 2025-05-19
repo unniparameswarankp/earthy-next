cb837bcd55e6b9f7d0ec9834fb080a620c2254de
jo <jyothishkpk@gmail.com>
2025-05-19 11:47:22 +0530

    idkhwbv

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,32 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import next from "eslint-plugin-next";
+import react from "eslint-plugin-react";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      react,
+      next
+    },
+    rules: {
+      "react/jsx-key": "error",
+      "react/react-in-jsx-scope": "off" // Needed for Next.js since React is auto-imported
+    }
+  }
+];
