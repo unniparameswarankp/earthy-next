7908a85718ba0f9d56c0d7097e688f91dff25c2e
jo <jyothishkpk@gmail.com>
2025-05-22 16:56:24 +0530

    test

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -1,108 +1,171 @@
-import { getAllCategorySlugs, getCategoryBySlug } from '../../lib/sanity';
+import { client, urlFor } from '../../lib/sanity';
 import iconImage from '../../assets/images/icon.png'; 
 import Link from 'next/link';
-import { urlFor } from '../../lib/sanity';
 import { useRouter } from 'next/router';
 
+const PRODUCTS_PER_PAGE = 6;
 
-
-
-
-export default function CategoryPage({ category }) {
+export default function CategoryPage({ category, currentPage, totalPages }) {
   const router = useRouter(); 
   const currentSlug = router.query.slug;
 
   if (!category) return <p>Loading...</p>; 
 
   return (
-    
-    <div  className='bk-product-list-page'>
-        <div className='bk-product-listing-banner'>
-            <div className='container'>
-            <h1>{category.title}</h1>
-            <p>Our cleaning chemicals are developed with biodegradable and non-toxic ingredients, ensuring they are safe for both users and the environment.</p>
-            </div>
-            <span className='down'><svg class="next-arrow-circle mt-5 scroll-to" data-scroll-target="#our-products" data-offset="-150" xmlns="http://www.w3.org/2000/svg" width="101" height="101" viewBox="0 0 101 101">                         <g transform="translate(-910 -573)">                             <g transform="translate(968.568 563.5) rotate(90)">                             <path d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16637.5 9976)" fill="none" stroke="#3a5322" stroke-linecap="round" stroke-width="2"></path>                             </g>                             <g transform="translate(910 573)" fill="none" stroke="#3a5322" stroke-width="2">                             <circle cx="50.5" cy="50.5" r="50.5" stroke="none"></circle>                             <circle cx="50.5" cy="50.5" r="49.5" fill="none"></circle>                             </g>                         </g>                     </svg></span>
-            <img className='icon-bg icon-1' src={iconImage.src} />
-            <img className='icon-bg icon-3' src={iconImage.src} />
-            <img className='icon-bg icon-5' src={iconImage.src} />
-            <img className='icon-bg icon-6' src={iconImage.src} />
-            <img className='icon-bg icon-8' src={iconImage.src} />
-            <img className='icon-bg icon-9' src={iconImage.src} />
-            <img className='icon-bg icon-10' src={iconImage.src} />
-        </div> 
-       <div className='container'>
+    <div className='bk-product-list-page'>
+      <div className='bk-product-listing-banner'>
+        <div className='container'>
+          <h1>{category.title}</h1>
+          <p>Our cleaning chemicals are developed with biodegradable and non-toxic ingredients, ensuring they are safe for both users and the environment.</p>
+        </div>
+        <span className='down'>
+          <svg className="next-arrow-circle mt-5 scroll-to" data-scroll-target="#our-products" data-offset="-150" xmlns="http://www.w3.org/2000/svg" width="101" height="101" viewBox="0 0 101 101">                         
+            <g transform="translate(-910 -573)">                             
+              <g transform="translate(968.568 563.5) rotate(90)">
+                <path d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16637.5 9976)" fill="none" stroke="#3a5322" strokeLinecap="round" strokeWidth="2" />
+              </g>                             
+              <g transform="translate(910 573)" fill="none" stroke="#3a5322" strokeWidth="2">
+                <circle cx="50.5" cy="50.5" r="50.5" stroke="none" />
+                <circle cx="50.5" cy="50.5" r="49.5" fill="none" />
+              </g>                         
+            </g>                     
+          </svg>
+        </span>
+        <img className='icon-bg icon-1' src={iconImage.src} />
+        <img className='icon-bg icon-3' src={iconImage.src} />
+        <img className='icon-bg icon-5' src={iconImage.src} />
+        <img className='icon-bg icon-6' src={iconImage.src} />
+        <img className='icon-bg icon-8' src={iconImage.src} />
+        <img className='icon-bg icon-9' src={iconImage.src} />
+        <img className='icon-bg icon-10' src={iconImage.src} />
+      </div>
+
+      <div className='container'>
         <ul className='category-listing'>
-            <li><Link  href="/shop">All Products</Link></li>
+          <li><Link href="/shop">All Products</Link></li>
           {category.allCategories.map((cat) => (
             <li key={cat.slug}>
               <Link
-                href={`/category/${cat.slug}`}
-                className={cat.slug === currentSlug ? 'active' : ''}
-              >
-                {cat.title}
-              </Link>
+  href={`/category/${cat.slug.current}`}
+  className={cat.slug.current === currentSlug ? 'active' : ''}
+>
+  {cat.title}
+</Link>
             </li>
           ))}
         </ul>
+
         <div className='row'>
-            {category.posts.map((post) => (
+          {category.posts.map((post) => (
             <div key={post.slug.current} className='col-md-6 col-12 pb-30'>
-                             <div className="bk-product-listing">
-                              <svg xmlns='http://www.w3.org/2000/svg' width='295.401' height='409.001' viewBox='0 0 295.401 409.001'><path id='Intersezione_1' data-name='Intersezione 1' d='M-9121,1999q-.388,0-.774-.01A30,30,0,0,1-9151,1969V1620a30,30,0,0,1,30-30h184.706c62.645,67.685,88.677,165.5,78.581,206.023-9.7,38.927-88.364,140.017-192.271,202.978Z' transform='translate(9151 -1589.999)' fill='#fffcd2'/></svg>
-                                <div className="row">
-                                  <div className="col-12 col-md-5">
-                                     <figure>
-                                        <img
-                                            src={urlFor(post.image).width(400).url()}
-                                            alt={post.title}
-                                        />
-                                     </figure>
-                                  </div>
-                                  <div className="col-12 col-md-7">
-                                     <div className="category">
-                                  
-                                     </div>
-                                     <div className="name">{post.title}</div>
-                                     <div className="category"> {typeof post.price === 'number'
-    ? new Intl.NumberFormat('en-NZ', {
-        style: 'currency',
-        currency: 'NZD',
-      }).format(post.price)
-    : 'Price not available'}</div>
-                                     <Link href={`/product/${post.slug.current}`} className="btn-1 green" tabindex="0">View Product<svg xmlns="http://www.w3.org/2000/svg" width="12.215" height="19.025" viewBox="0 0 12.215 19.025"><path id="arrow" d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16581.229 9977.23)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2"></path></svg></Link>
-                                  </div>
-                                </div>
-                              </div>
+              <div className="bk-product-listing">
+                <svg xmlns='http://www.w3.org/2000/svg' width='295.401' height='409.001' viewBox='0 0 295.401 409.001'><path id='Intersezione_1' data-name='Intersezione 1' d='M-9121,1999q-.388,0-.774-.01A30,30,0,0,1-9151,1969V1620a30,30,0,0,1,30-30h184.706c62.645,67.685,88.677,165.5,78.581,206.023-9.7,38.927-88.364,140.017-192.271,202.978Z' transform='translate(9151 -1589.999)' fill='#fffcd2'/></svg>
+                <div className="row">
+                  <div className="col-12 col-md-5">
+                    <figure>
+                      <img
+                        src={urlFor(post.image).width(400).url()}
+                        alt={post.title}
+                      />
+                    </figure>
+                  </div>
+                  <div className="col-12 col-md-7">
+                    <div className="category"></div>
+                    <div className="name">{post.title}</div>
+                    <div className="category">
+                      {typeof post.price === 'number'
+                        ? new Intl.NumberFormat('en-NZ', {
+                            style: 'currency',
+                            currency: 'NZD',
+                          }).format(post.price)
+                        : 'Price not available'}
+                    </div>
+                    <Link href={`/product/${post.slug.current}`} className="btn-1 green" tabIndex="0">
+                      View Product
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12.215" height="19.025" viewBox="0 0 12.215 19.025">
+                        <path id="arrow" d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16581.229 9977.23)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="2"></path>
+                      </svg>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
-            ))}
+          ))}
         </div>
-       </div>
+
+        {/* Pagination */}
+        {totalPages > 1 && (
+          <div className="pagination">
+            {Array.from({ length: totalPages }).map((_, index) => {
+              const pageNum = index + 1;
+              return (
+                <Link
+                  key={pageNum}
+                  href={`/category/${currentSlug}?page=${pageNum}`}
+                  className={pageNum === currentPage ? 'active' : ''}
+                >
+                  {pageNum}
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
+export async function getServerSideProps(context) {
+  const { slug } = context.params;
+  const page = parseInt(context.query.page || '1');
+  const start = (page - 1) * PRODUCTS_PER_PAGE;
 
-// Pre-build paths for each category
-export async function getStaticPaths() {
-  const slugs = await getAllCategorySlugs();
-  const paths = slugs.map((slug) => ({
-    params: { slug: slug.slug },
-  }));
+  const categoryQuery = `
+    *[_type == "category" && slug.current == $slug][0]{
+      title,
+      slug,
+      "posts": *[_type == "product" && references(^._id)] | order(_createdAt desc) [${start}...${start + PRODUCTS_PER_PAGE}] {
+        title,
+        slug,
+        price,
+        image
+      }
+    }
+  `;
 
-  return {
-    paths,
-    fallback: true, // or false or 'blocking'
-  };
-}
+  const countQuery = `
+    count(*[_type == "product" && references(*[_type == "category" && slug.current == $slug]._id)])
+  `;
 
-// Fetch data for each category page
-export async function getStaticProps({ params }) {
-  const category = await getCategoryBySlug(params.slug);
+  const allCategoriesQuery = `
+    *[_type == "category"]{
+      title,
+      slug
+    }
+  `;
+
+  const [categoryData, totalCount, allCategories] = await Promise.all([
+    client.fetch(categoryQuery, { slug }),
+    client.fetch(countQuery, { slug }),
+    client.fetch(allCategoriesQuery),
+  ]);
+
+  const totalPages = Math.ceil(totalCount / PRODUCTS_PER_PAGE);
+
+  if (!categoryData) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
-      category,
+      category: {
+        ...categoryData,
+        posts: categoryData.posts || [],
+        allCategories,
+      },
+      currentPage: page,
+      totalPages,
     },
-    revalidate: 60, // ISR: rebuild every 60s
   };
 }
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,50 +1,55 @@
-// pages/shop.js
-
-import Image from 'next/image';
 import Link from 'next/link';
 import iconImage from '../assets/images/icon.png';
-import ProductImage from '../assets/images/product.png';
-import { client } from '@/lib/sanity'; // adjust path if needed
-import { urlFor } from '@/lib/image'; 
+import { client } from '@/lib/sanity';
+import { urlFor } from '@/lib/image';
 
-export async function getServerSideProps() {
-  // Fetch all products and their categories
-  const productsQuery = `*[_type == "product"]{
-  _id,
-  title,
-  slug, // ✅ Add this line
-  price,
-  "categories": categories[]->{
+const PRODUCTS_PER_PAGE = 6;
+
+export async function getServerSideProps(context) {
+  const page = parseInt(context.query.page || '1');
+  const start = (page - 1) * PRODUCTS_PER_PAGE;
+
+  const productsQuery = `*[_type == "product"] | order(_createdAt desc) [${start}...${start + PRODUCTS_PER_PAGE}]{
     _id,
     title,
-    slug
-  },
-  image
-}`;
+    slug,
+    price,
+    "categories": categories[]->{
+      _id,
+      title,
+      slug
+    },
+    image
+  }`;
 
-
+  const totalCountQuery = `count(*[_type == "product"])`;
   const categoriesQuery = `*[_type == "category"]{
     _id,
     title,
     slug
   }`;
 
-  const products = await client.fetch(productsQuery);
-  const categories = await client.fetch(categoriesQuery);
+  const [products, totalCount, categories] = await Promise.all([
+    client.fetch(productsQuery),
+    client.fetch(totalCountQuery),
+    client.fetch(categoriesQuery),
+  ]);
+
+  const totalPages = Math.ceil(totalCount / PRODUCTS_PER_PAGE);
 
   return {
     props: {
       products,
       categories,
+      totalPages,
+      currentPage: page,
     },
   };
 }
 
-const Shop = ({ products, categories }) => {
-
-  
+const Shop = ({ products, categories, totalPages, currentPage }) => {
   return (
-    <div  className='bk-product-list-page'>
+    <div className="bk-product-list-page">
         <div className='bk-product-listing-banner'>
             <div className='container'>
             <h1>Our Products</h1>
@@ -59,57 +64,64 @@ const Shop = ({ products, categories }) => {
             <img className='icon-bg icon-9' src={iconImage.src} />
             <img className='icon-bg icon-10' src={iconImage.src} />
         </div> 
-       <div className='container'>
-        <ul className='category-listing'>
-            {/* 
-            <li><Link href="/category/chemical">Chemical</Link></li>
-            <li><Link href="/category/compostable-bags">Compostable Bags</Link></li>
-            <li><Link href="/category/heavy-duty-wipes">Heavy Duty Wipes</Link></li>
-            <li><Link href="/category/rolls">Rolls</Link></li>
-            <li><Link href="/category/urinal-screen">Urinal Screen</Link></li> */}
-            <li><Link className='active' href="/shop">All Products</Link></li>
-            {categories.map(cat => (
+       <div className="container">
+        <ul className="category-listing">
+          <li>
+            <Link className="active" href="/shop">All Products</Link>
+          </li>
+          {categories.map((cat) => (
             <li key={cat._id}>
-              {cat.slug?.current ? (
-                <Link href={`/category/${cat.slug.current}`}>
-                  {cat.title}
-                </Link>
-              ) : (
-                <span>{cat.title}</span>
-              )}
+              <Link href={`/category/${cat.slug.current}`}>
+                {cat.title}
+              </Link>
             </li>
           ))}
-
         </ul>
-        <div className='row'>
-            {products.map(prod => (
-            <div key={prod._id} className='col-md-6 col-12 pb-30'>
-                             <div className="bk-product-listing">
-                              <svg xmlns='http://www.w3.org/2000/svg' width='295.401' height='409.001' viewBox='0 0 295.401 409.001'><path id='Intersezione_1' data-name='Intersezione 1' d='M-9121,1999q-.388,0-.774-.01A30,30,0,0,1-9151,1969V1620a30,30,0,0,1,30-30h184.706c62.645,67.685,88.677,165.5,78.581,206.023-9.7,38.927-88.364,140.017-192.271,202.978Z' transform='translate(9151 -1589.999)' fill='#fffcd2'/></svg>
-                                <div className="row">
-                                  <div className="col-12 col-md-5">
-                                     <figure>
-                                        {prod.image && <img src={urlFor(prod.image).width(200).url()} alt={prod.title} />}
-                                     </figure>
-                                  </div>
-                                  <div className="col-12 col-md-7">
-                                    
-                                     <div className="name">{prod.title}</div>
-                                     <div className="category">{new Intl.NumberFormat('en-NZ', {
-                                                             style: 'currency',
-                                                             currency: 'NZD'
-                                                           }).format(prod.price)}</div>
-                                     <Link href={`/product/${prod.slug.current}`} className="btn-1 green" tabindex="0">View Product<svg xmlns="http://www.w3.org/2000/svg" width="12.215" height="19.025" viewBox="0 0 12.215 19.025"><path id="arrow" d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16581.229 9977.23)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2"></path></svg></Link>
-                                  </div>
-                                </div>
-                              </div>
+
+        <div className="row">
+          {products.map((prod) => (
+            <div key={prod._id} className="col-md-6 col-12 pb-30">
+              <div className="bk-product-listing">
+                <div className="row">
+                  <div className="col-12 col-md-5">
+                    <figure>
+                      {prod.image && <img src={urlFor(prod.image).width(200).url()} alt={prod.title} />}
+                    </figure>
+                  </div>
+                  <div className="col-12 col-md-7">
+                    <div className="name">{prod.title}</div>
+                    <div className="category">
+                      {new Intl.NumberFormat('en-NZ', {
+                        style: 'currency',
+                        currency: 'NZD'
+                      }).format(prod.price)}
+                    </div>
+                    <Link href={`/product/${prod.slug.current}`} className="btn-1 green">View Product</Link>
+                  </div>
+                </div>
+              </div>
             </div>
-            ))}
+          ))}
         </div>
-       </div>
+
+        {/* 🔽 Pagination */}
+        <div className="pagination">
+          {Array.from({ length: totalPages }).map((_, index) => {
+            const pageNum = index + 1;
+            return (
+              <Link
+                key={pageNum}
+                href={`/shop?page=${pageNum}`}
+                className={pageNum === currentPage ? 'active' : ''}
+              >
+                {pageNum}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Shop;
-
-
