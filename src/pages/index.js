6e5dab7346ba4ef46df603d24abc82caa16803ac
jo <jyothishkpk@gmail.com>
2025-06-10 12:23:03 +0530

    yyy

--- src/pages/index.js
+++ src/pages/index.js
@@ -286,23 +286,21 @@ const Home = ({ products }) => {
       </section>
 
 
-      <section className="bk-home-content-section position-relative">
+      <section className="bk-home-content-section position-relative d-none">
         <div className="description-bk">
          <div className="row position-relative">
           <div className="col-md-8">
-             <div ref={textRef2} className="text-bk">
+             <div className="text-bk">
                 <svg className="bubble-desktop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1155.519 989.7">                         <path id="bubble-01" d="M727.546,58.656c310.955,0,449.911,393.087,420.51,513.555s-406.925,561.318-765.145,461.58S-18.559,637.168-.059,417.165,416.591,58.656,727.546,58.656Z" transform="translate(3.614 -58.656)" fill="#fff"></path>                     </svg>
                 <div className="wrap">
                   <h2>Eco-power in every drop.</h2>
-                  <h5> At NZ Cleaning Supplies, we don't just aim to meet expectations—we aim to exceed them. </h5>
-                  {/* <p>Every product we create or distribute reflects our commitment to a cleaner planet. From biodegradable chemicals to energy-efficient equipment, we lead the charge in eco-friendly innovation. We understand that no two businesses are the same. That's why we offer tailored products and services that fit seamlessly into your operations, boosting efficiency and results. </p> */}
-                  <Link className="btn-1 green" href="/about-us">About Us<svg xmlns="http://www.w3.org/2000/svg" width="12.215" height="19.025" viewBox="0 0 12.215 19.025"><g transform="translate(-56.271 1.23)"><path d="M-16580-9976a12.318,12.318,0,0,0,2.834,4.833,19.363,19.363,0,0,0,5.449,3.451,18.406,18.406,0,0,0-5.449,3.383,12.344,12.344,0,0,0-2.834,4.9" transform="translate(16637.5 9976)" fill="none" stroke="#3a5322" stroke-linecap="round" stroke-width="2"></path></g></svg></Link>
+                  <p>At Earthy, we believe that real cleaning power doesn't have to come from harsh chemicals. Our plant-based formulas are concentrated with natural, biodegradable ingredients that pack a serious punch — all while being safe for your home and gentle on the planet. Every drop is crafted to cut through dirt, grease, and germs without leaving behind toxins or waste. It's proof that sustainable cleaning can be powerful, effective, and beautifully simple.</p>
                 </div>
              </div>
           </div>
           <div className="col-md-4">
            <figure>
-             <img ref={sideimgRef2} src={cleanerImage.src}/>
+             <img src={cleanerImage.src}/>
            </figure>
           </div>
           </div>
@@ -312,7 +310,23 @@ const Home = ({ products }) => {
         <img className="icon-leaf-float icon-leaf-5" src={iconImage.src} alt="Leaf Icon" />
       </section>
 
-      <section className='bk-home-not'>
+      <section className='bk-home-not pt-100'>
+        <div className='container'>
+          <div className='row justify-content-center align-items-center flex-direction-md-reverce'>
+            <div className='col-md-5 col-12'>
+              <figure>
+                <img  src={cleanerImage.src}/>
+              </figure>
+            </div>
+            <div className='col-md-7 col-12'>
+              <h2>Eco-power in every drop.</h2>
+              <p>At Earthy, we believe that real cleaning power doesn't have to come from harsh chemicals. Our plant-based formulas are concentrated with natural, biodegradable ingredients that pack a serious punch — all while being safe for your home and gentle on the planet. Every drop is crafted to cut through dirt, grease, and germs without leaving behind toxins or waste. It's proof that sustainable cleaning can be powerful, effective, and beautifully simple.</p>
+            </div>
+          </div>
+        </div>
+       </section>
+
+      <section className='bk-home-not pt-50'>
         <div className='container'>
           <div className='row justify-content-center align-items-center'>
             <div className='col-md-5 col-12'>
